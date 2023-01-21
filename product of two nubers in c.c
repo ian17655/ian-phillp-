e7d1b283c70db5c99a48a15468c430ae43d6b73a
ian17655 <122543828+ian17655@users.noreply.github.com>
2023-01-21 23:06:41 +0300

    Add files via upload

---
+++ product of two nubers in c.c
@@ -0,0 +1,13 @@
+//program to compute product of two numbers
+#include<stdio.h>
+int main
+{
+int a=0,b=0;
+printf("/nEnter the value of a");
+scanf("%d",&a);
+printf("/nEnter the value of b");
+scanf("%d",&b);
+area=a*b;
+printf("The product of two numbers=%d"area);
+
+return 0;}
